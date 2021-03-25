13830d7f0b34becee3ed5aacd54ae980226d7e6c
OPOII <41926039+OPOII@users.noreply.github.com>
2021-03-25 00:20:50 -0500

    Changes to deployment

--- src/app/pages/dashboard/services/books.service.ts
+++ src/app/pages/dashboard/services/books.service.ts
@@ -2,15 +2,15 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { IBook } from '../interfaces/IBook';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-//import { environment } from 'src/environments/environment';
-import { environment } from 'src/environments/environment.prod'
+import { environment } from 'src/environments/environment';
+import { environmentProd } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
-  //urlBase = environment.baseUrl;
-  urlBaseProd=environment.baseUrl;
+  urlBase = environment.baseUrl;
+  urlBaseProd=environmentProd.baseUrl;
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
@@ -18,18 +18,18 @@ export class BooksService {
   constructor(private http: HttpClient) { }
 
   getBooks(): Observable<IBook[]> {
-    return this.http.get<IBook[]>(`${this.urlBaseProd}/books`);
+    return this.http.get<IBook[]>(`${this.urlBase}/books`);
   }
 
   deleteBook(id: string) {
-    return this.http.delete(`${this.urlBaseProd}/books/${id}`);
+    return this.http.delete(`${this.urlBase}/books/${id}`);
   }
 
   createBook({name, author}: { name: string, author: string}) {
-    return this.http.post<IBook>(`${this.urlBaseProd}/books`, { name, author }, this.httpOptions);
+    return this.http.post<IBook>(`${this.urlBase}/books`, { name, author }, this.httpOptions);
   }
 
   updateBook({id, name, author}: { id: string, name: string, author: string}) {
-    return this.http.put<IBook>(`${this.urlBaseProd}/books/${id}`, { name, author }, this.httpOptions);
+    return this.http.put<IBook>(`${this.urlBase}/books/${id}`, { name, author }, this.httpOptions);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const environment = {
+export const environmentProd = {
   production: true,
   baseUrl: 'https://electivacicd-icesi.herokuapp.com/',
 };
